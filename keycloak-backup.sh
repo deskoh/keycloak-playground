5661328b04395ac2503ebad091a94b582e436645
Desmond Koh <deskoh.sg@gmail.com>
2021-03-03 16:22:20 +0800

    Create keycloak-backup.sh

---
+++ keycloak-backup.sh
@@ -0,0 +1,18 @@
+# Backup keycloak config to `keycloak-backup` directory
+# Note: User `admin` will be created it it exists, ignored otherwise. Can specify any existing user with aribtrary password as well
+mkdir keycloak-backup
+chmod 777 keycloak-backup
+
+docker run --rm\
+    --name keycloak_exporter \
+    --network keycloak-network \
+    -v $(pwd)/keycloak-backup:/tmp/realm-config:Z\
+    -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=password \
+    -e DB_VENDOR=mariadb -e DB_ADDR=mariadb -e DB_USER=keycloak -e DB_PASSWORD=password \
+    -e X509_CA_BUNDLE=/tmp/certs/ca.crt \
+    -v $(pwd)/certs/rootCA.crt:/tmp/certs/ca.crt \
+    jboss/keycloak:11.0.1 \
+    -Dkeycloak.migration.action=export \
+    -Dkeycloak.migration.provider=dir \
+    -Dkeycloak.migration.dir=/tmp/realm-config \
+    -Dkeycloak.migration.usersExportStrategy=SAME_FILE
